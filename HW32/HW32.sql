39fcc6f683d685465929345f10ab15d6fb73ac0b
YanaImbras <archangel_13@mail.ru>
2024-09-22 18:51:33 +0300

    HW21 DLL

--- HW32/HW32.sql
+++ HW32/HW32.sql
@@ -30,28 +30,52 @@ GROUP BY ord.CustomerID, det.StockItemID
 ORDER BY ord.CustomerID, det.StockItemID
 
 --Оптимизированный текст запроса
+--SELECT 
+--	ord.CustomerID, 
+--	det.StockItemID, 
+--	SUM(det.UnitPrice), 
+--	SUM(det.Quantity), 
+--	COUNT(ord.OrderID)
+--FROM Sales.Orders AS ord
+--	JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID
+--	JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID 
+--									AND Inv.BillToCustomerID != ord.CustomerID --вынесено
+----	JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID --не используется. убираем
+--	JOIN Warehouse.StockItems AS It ON It.StockItemID = det.StockItemID 
+--									AND It.SupplierId = 12 --вынесено из доп.селект
+----	JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID --не используется. убираем
+--WHERE DATEDIFF(DAY, Inv.InvoiceDate, ord.OrderDate) = 0
+--AND (
+--        SELECT 
+--			SUM(Total.UnitPrice*Total.Quantity)
+--        FROM Sales.OrderLines AS Total
+--			JOIN Sales.Orders AS ordTotal ON ordTotal.OrderID = Total.OrderID
+--        WHERE ordTotal.CustomerID = ord.CustomerID
+--        GROUP BY ordTotal.CustomerID
+--    ) > 250000
+--GROUP BY ord.CustomerID, det.StockItemID
+--ORDER BY ord.CustomerID, det.StockItemID
+
+
 SELECT 
 	ord.CustomerID, 
 	det.StockItemID, 
 	SUM(det.UnitPrice), 
 	SUM(det.Quantity), 
 	COUNT(ord.OrderID)
-FROM Sales.Orders AS ord
+FROM 
+	Sales.Orders AS ord
 	JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID
-	JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID 
-									AND Inv.BillToCustomerID != ord.CustomerID --вынесено
---	JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID --не используется. убираем
+	JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID
+	JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID 
+	JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID 
 	JOIN Warehouse.StockItems AS It ON It.StockItemID = det.StockItemID 
-									AND It.SupplierId = 12 --вынесено из доп.селект
---	JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID --не используется. убираем
-WHERE DATEDIFF(DAY, Inv.InvoiceDate, ord.OrderDate) = 0
-AND (
-        SELECT 
-			SUM(Total.UnitPrice*Total.Quantity)
-        FROM Sales.OrderLines AS Total
-			JOIN Sales.Orders AS ordTotal ON ordTotal.OrderID = Total.OrderID
-        WHERE ordTotal.CustomerID = ord.CustomerID
-        GROUP BY ordTotal.CustomerID
-    ) > 250000
+									AND It.SupplierId = 12
+	JOIN Sales.OrderLines AS Total ON Total.OrderID = ord.OrderID
+	JOIN Sales.Orders AS ordTotal ON ordTotal.OrderID = Total.OrderID
+WHERE 
+	Inv.BillToCustomerID != ord.CustomerID
+	AND (Total.UnitPrice * Total.Quantity) > 250000
+	AND Inv.InvoiceDate = ord.OrderDate
 GROUP BY ord.CustomerID, det.StockItemID
 ORDER BY ord.CustomerID, det.StockItemID
